7aea797a7d97c523fab49bddc5e4ec14de745b03
Mike Harris <mike.a.harris.13@gmail.com>
2025-11-30 10:25:14 -0600

    remove restraint on which maps should have test cases run

--- src/benchmark.ts
+++ src/benchmark.ts
@@ -7,19 +7,17 @@ import { runAStarBenchmark } from "./benchmarkAStar.ts";
 export function runBenchmarks() {
     const mapCaseFiles = discoverCases();
     for (let i = 0; i < mapCaseFiles.length; i++) {
-        if (i == 0) {
-            const fileContents = fs.readFileSync(mapCaseFiles[i], "utf8");
-            const mapCase: MapCases = JSON.parse(fileContents);
-            const map = loadMapFromName(mapCase.mapName);
-            const miniMap = loadMiniMapFromName(mapCase.mapName);
-            if (map === null || miniMap === null) {
-                console.warn(`Was unable to load maps for ${mapCase.mapName}`);
-                continue;
-            }
-            const tests = mapCase.cases;
-            const v = mapCase.version;
-            runBFSBenchmark(map, tests, v);
-            runAStarBenchmark(map, miniMap, tests, v);
+        const fileContents = fs.readFileSync(mapCaseFiles[i], "utf8");
+        const mapCase: MapCases = JSON.parse(fileContents);
+        const map = loadMapFromName(mapCase.mapName);
+        const miniMap = loadMiniMapFromName(mapCase.mapName);
+        if (map === null || miniMap === null) {
+            console.warn(`Was unable to load maps for ${mapCase.mapName}`);
+            continue;
         }
+        const tests = mapCase.cases;
+        const v = mapCase.version;
+        runBFSBenchmark(map, tests, v);
+        runAStarBenchmark(map, miniMap, tests, v);
     }
 }
